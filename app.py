942eacef6868d8702fa3f1caa1d49adf8702286b
Aymerick Michelet <micheletaymerick@gmail.com>
2022-04-16 17:31:41 +0200

    WIP

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +7,21 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template("index.html")
+
+
+@app.route('/parse-csv', methods=['POST', 'GET'])
+def parse_csv():
+
+    # if request.method == 'POST':
+    #     print(request.files)
+    #     file = request.files['file']
+    #     file.save(os.path.join('upload', file.filename))
+
+    response = {
+        'message': 'wesh',
+        'status': 200
+    }
+    return jsonify(response)
 
 
 if __name__ == '__main__':
---
+++ static/script.js
@@ -0,0 +1,80 @@
+// Steps: BEFORE=0/LOADING=1/RESULT=2
+let index = 0;
+
+document.querySelector('#upload')
+    .addEventListener('change', event => {
+        // parseCsv(event)
+        document.querySelector('#formUpload').submit();
+    })
+
+
+const parseCsv = () => {
+    const files = document.querySelector('#upload')
+    const formData = new FormData()
+    formData.append('myFile', files.files[0])
+
+    const requestOptions = {
+        // headers: {
+        //     "Content-Type": "multipart/form-data",
+        // },
+        // mode: "no-cors",
+        method: "POST",
+        // files: files.files[0],
+        body: formData,
+    }
+
+    console.log(requestOptions)
+    console.log(files.files[0])
+
+    fetch('/parse-csv', requestOptions).then(
+        (response) => {
+            console.log(response.data);
+        }
+    )
+
+    // fetch('/parse-csv', {
+    //     headers: {
+    //         'Content-Type': 'text/csv'
+    //     },
+    //     method: 'POST',
+    //     body: formData
+    // })
+    //     .then(response => response.text())
+    //     .then(data => {
+    //         console.log(data)
+    //     })
+    //     .catch(error => {
+    //         console.error(error)
+    //     })
+
+//    test await + try catch
+}
+
+const displayStep = () => {
+    const inner_square = document.getElementById("inner_square")
+    const loader = document.getElementById("loader")
+    const upload = document.getElementById("upload")
+    const csv_icon = document.getElementById("csv_icon")
+    const label = document.getElementById("label")
+
+    if (!index) { // index === 0
+        inner_square.classList.add("pointer")
+        loader.classList.add("hide")
+        upload.removeAttribute("disabled")
+        csv_icon.classList.remove("hide")
+        label.classList.remove("hide")
+    } else if (index === 1) {
+        inner_square.classList.remove("pointer")
+        loader.classList.remove("hide")
+        upload.setAttribute("disabled", "")
+        csv_icon.classList.add("hide")
+        label.classList.add("hide")
+    }
+}
+
+const switchStep = () => {
+    index < 2 ? index++ : index = 0
+    displayStep()
+}
+
+displayStep()
